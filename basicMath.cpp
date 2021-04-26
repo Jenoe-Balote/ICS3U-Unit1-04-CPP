5769767c47b42c05ee044d6a0ffa90f5431aea3a
Ubuntu <ubuntu@ip-172-31-49-223.ec2.internal>
2021-04-26 14:59:50 +0000

    added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 Jenoe Balote All rights reserved
+//
+// Created by Jenoe Balote
+// Created on April 2021
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" << (9+2) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+4/2=" << (4+4/2) << std::endl;
+    std::cout << "3+4*2=" << (3+4*2) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5+pow(2,3)) << std::endl;
+}
